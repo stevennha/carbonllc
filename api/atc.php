6d448627819a25f0b59a04548919fcc2e8ae620c
Steven751-ui <109867418+Steven751-ui@users.noreply.github.com>
2022-09-27 22:46:59 -0500

    Initial Upload

---
+++ api/atc.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_GET["id"])) {
+  echo "<script>var _0x32ef=['https://www.off---white.com/en/GB/frame_increment_item_quantity_from_cart?variant_id=".$_GET['id']."','https://www.off---white.com/en/GB/checkout/address','location','href'];(function(_0x409ec1,_0x3dee3f){var _0x50e382=function(_0x491b8e){while(--_0x491b8e){_0x409ec1['push'](_0x409ec1['shift']());}};_0x50e382(++_0x3dee3f);}(_0x32ef,0x6a));var _0x7451=function(_0x588aa0,_0x4b33db){_0x588aa0=_0x588aa0-0x0;var _0x1214c3=_0x32ef[_0x588aa0];return _0x1214c3;};function wait(_0x596a7c){return new Promise(_0x59779b=>setTimeout(_0x59779b,_0x596a7c));};async function start(){window[_0x7451('0x0')][_0x7451('0x1')]=_0x7451('0x2');await wait(0x226);window[_0x7451('0x0')][_0x7451('0x1')]=_0x7451('0x3');};start();</script>";
+} else {
+  echo "Wrong Parameters!";
+}
+?>
